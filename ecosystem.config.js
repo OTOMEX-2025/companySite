29e6426adf7398cc43f0596a4a97edf07ef0d062
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2025-11-03 16:03:12 +0200

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+// ecosystem.config.js
+module.exports = {
+  apps: [{
+    name: 'Otomex Innovations',
+    script: 'npm',
+    args: 'start',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+}
